01e0bf87cfcab3578706822b8e6dd1e55f046e6b
ShawnBorn-Newton <shawn.newton3@gmail.com>
2017-07-12 12:01:49 -0400

    movie problem

--- Lists/Program.cs
+++ Lists/Program.cs
@@ -54,12 +54,64 @@ namespace Lists
 
             //}
 
-            List<string> favFooods = new List<string> { "Pizza", "Lo Mian", "Sushi Maki", "Unagi" };
+            //    List<string> favFooods = new List<string> { "Pizza", "Lo Mian", "fish", "Sushi Maki", "Unagi" };
 
-            if(favFooods.Contains("fish"))
+            //    if(favFooods.Contains("fish"))
+            //    {
+            //        Console.WriteLine("I like fish too!");
+            //    }
+
+            //List<int> numbersList = new List<int>{ 1, 23, 9, 77, 922, 6, 32, 63, 14, 5 };
+            //if(numbersList.Contains (23))
+            //{
+            //    numbersList.Remove (23);
+            //    Console.WriteLine("removeing 23");
+            //}
+            //if (numbersList.Contains(77))
+            //{
+            //    numbersList.Remove(77);
+            //    Console.WriteLine("removeing 77");
+            //}
+            //if (numbersList.Contains(15))
+            //{
+            //    numbersList.Remove(15);
+            //    Console.WriteLine("removeing 15");
+            //}
+            //Console.WriteLine("Also remoeing 6 and 32");
+            //numbersList.Remove(6);
+            //numbersList.Remove(32);
+            //foreach (int number in numbersList)
+            //{
+            //    Console.WriteLine(number);
+            //}
+            List<string> movies = new List<string> {"jay and silent bob", "mr. bill" };
+            bool quite = false;
+            do
             {
-                Console.WriteLine("I like fish too!");
+                Console.WriteLine("Please enter a movie so we can look thourgh our stock.\n type esc to quite.");
+                string responseMovie = Console.ReadLine().ToLower();
+
+                if (movies.Contains(responseMovie))
+                {
+                    Console.WriteLine("We got it!");
+                }
+                if (responseMovie == "esc")
+                {
+                    quite = true;
+                    Console.WriteLine("Movies being ordered and Movies we have:");
+                    foreach(string movie in movies)
+                    {
+                        Console.WriteLine(movie);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("We are going to order that for you, hold tight.");
+                    movies.Add(responseMovie);
+                }
+                
             }
+            while (quite == false);
         }
     }
 }
